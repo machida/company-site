b12fb6355468a30208bc80b5f9c4e719ec979364
teppei machida <machidanohimitsu@gmail.com>
2014-04-23 20:38:20 +0900

    Change gem for media query

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module CompanySite
     config.compass.require 'SassyLists'
     config.compass.require 'ceaser-easing'
     config.compass.require 'rgbapng'
-    config.compass.require 'sass-mediaqueries-rails'
+    config.compass.require 'breakpoint'
 
     config.assets.initialize_on_precompile = false
     config.assets.precompile += ['jquery.js']
